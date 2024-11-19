520b7f1b9f74c913138536da26f0cc83a2803c0c
Max <magmacube@mail.ru>
2024-11-20 02:16:10 +0400

    обновление

--- src/project/api/depends.py
+++ src/project/api/depends.py
@@ -1,6 +1,8 @@
 from project.infrastructure.postgres.repository.user_repo import UserRepository
 from project.infrastructure.postgres.database import PostgresDatabase
+from project.infrastructure.postgres.repository.flowers_repo import FlowerRepository
 
 
 user_repo = UserRepository()
+flowers_repo = FlowerRepository()
 database = PostgresDatabase()
