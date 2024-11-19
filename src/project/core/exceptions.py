b5102fb13d044293cfc9511ff6e2809139c63c54
Max <magmacube@mail.ru>
2024-11-20 02:15:56 +0400

    ~Ошибки, скорее всего правильно~

--- src/project/core/exceptions.py
+++ src/project/core/exceptions.py
@@ -16,3 +16,24 @@ class UserAlreadyExists(BaseException):
     def __init__(self, email: str) -> None:
         self.message = self._ERROR_MESSAGE_TEMPLATE.format(email=email)
         super().__init__(self.message)
+
+class FlowerAlreadyExists(BaseException):
+    _ERROR_MESSAGE_TEMPLATE: Final[str] = "Такой цветок уже есть"
+
+    def __init__(self, name: str) -> None:
+        self.message = self._ERROR_MESSAGE_TEMPLATE.format(name=name)
+        super().__init__(self.message)
+
+class FlowerNotFound(BaseException):
+    "Исключение, вызываемое, когда цветок не найден."
+    _ERROR_MESSAGE_TEMPLATE: Final[str] = "Цветок с id {id} не найден"
+    message: str
+
+    def __init__(self, _id: int) -> None:
+        self.message = self._ERROR_MESSAGE_TEMPLATE.format(id=_id)
+        super().__init__(self.message)
+
+class FlowerNotFound(Exception):
+    def __init__(self, message="Цветок не найден"):
+        self.message = message
+        super().__init__(self.message)
